4612d96d29eaa7cef72b044bae7045aa49ac85fc
Maximus-uk-25 <udhaya1606sp@gmail.com>
2025-06-08 23:46:10 +0530

    Master Commit

--- src/com/temenos/l3/DevJava1.java
+++ src/com/temenos/l3/DevJava1.java
@@ -2,5 +2,6 @@ public class DevJava1 {
     
     public static void main(String[] args) {
         System.out.println("javaFile-1121");
+        System.out.println("code on master branch");
     }
 }
