f8e9ba9543072f280038510f75b4f344051fc2d0
Maximus-uk-25 <udhaya1606sp@gmail.com>
2025-06-09 00:58:08 +0530

    dev branch commit c2

--- src/com/temenos/l3/DevJava1.java
+++ src/com/temenos/l3/DevJava1.java
@@ -3,5 +3,6 @@ public class DevJava1 {
     public static void main(String[] args) {
         System.out.println("javaFile-1121");
         System.out.println("javaFile-in branch Development");
+        System.out.println("code on master branch");
     }
 }
