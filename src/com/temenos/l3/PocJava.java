49876112561a31585237a348c7e1c9ff27b116e2
Maximus-uk-25 <udhaya1606sp@gmail.com>
2025-06-09 01:41:07 +0530

    Master Pocjava diff

--- src/com/temenos/l3/PocJava.java
+++ src/com/temenos/l3/PocJava.java
@@ -3,5 +3,7 @@ package com.temenos.l3;
 public class PocJava {
     public static void main(String[] args) {
     System.out.println("code on poc branch pocjava");
+    System.out.println("Line2");
+    System.out.println("Line3");
     }
 }
